6c09fc3b1121380a87d587b006e346e09bf21f9f
LoanThai <thloanth.dut@gmail.com>
2021-07-07 15:52:44 +0700

    Modify AppBarLabel and customAppBar

--- Flutter_Weather/lib/widgets/custom_app_bar.dart
+++ Flutter_Weather/lib/widgets/custom_app_bar.dart
@@ -1,17 +1,24 @@
 import 'package:flutter_weather/resources/palette.dart';
+import 'package:flutter_weather/services/date_formatter.dart';
 import 'package:flutter_weather/widgets/labels/label.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class AppBarLabel extends StatelessWidget {
-  final String? text;
-  AppBarLabel({required this.text});
+  final String city;
+  AppBarLabel({required this.city});
   @override
   Widget build(BuildContext context) {
-    return Label(
-      text: text ?? '',
-      weight: FontWeight.w600,
-      size: 18,
+    return RichText(
+        text: TextSpan(
+            children: [
+              TextSpan(
+                  text:"Da Nang City",
+                  style: TextStyle(fontSize: 25)
+              ),
+              TextSpan(text: "\n${DateFormatter.dateTime(DateTime.now())}")
+            ]
+        )
     );
   }
 }
@@ -28,10 +35,10 @@ class CustomAppBar extends AppBar {
       : super(
             backwardsCompatibility: false,
             systemOverlayStyle:
-                SystemUiOverlayStyle(statusBarColor: Colors.red),
+                SystemUiOverlayStyle(statusBarColor: Palette.primary),
             centerTitle: true,
             title: title,
-            backgroundColor: backgroundColor,
+            backgroundColor: Palette.transparent,
             elevation: 0,
             leading: leading,
             leadingWidth: leadingWidth,
