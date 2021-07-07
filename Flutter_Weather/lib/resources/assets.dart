8f0db08efc3ce07dae49f03b5fae0385dc8eda00
LoanThai <thloanth.dut@gmail.com>
2021-07-07 17:18:41 +0700

    Add icons

--- Flutter_Weather/lib/resources/assets.dart
+++ Flutter_Weather/lib/resources/assets.dart
@@ -1,4 +1,6 @@
 class Assets {
   // static const String IMAGE_LOGO = './assets/icons/logo.svg';
-  static const String WeatherImage = './assets/images/weather.jpg';
+  static const String weatherImage = './assets/images/weather.jpg';
+  static const String avatarPlaceholder = './assets/icons/avatar-placeholder.png';
+  static const String gmailLogo = "./assets/icons/gmail-logo.jpg";
 }
