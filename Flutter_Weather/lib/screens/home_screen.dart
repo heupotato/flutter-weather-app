79248159beee7dece097206a1a4a557a49f48bc3
LoanThai <thloanth.dut@gmail.com>
2021-07-08 09:54:55 +0700

    Update home_screen.dart

--- Flutter_Weather/lib/screens/home_screen.dart
+++ Flutter_Weather/lib/screens/home_screen.dart
@@ -40,6 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    WeatherDataRepository.printJson().then(print);
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: CustomAppBar(
