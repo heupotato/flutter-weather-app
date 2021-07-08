8bf053e628cb90dd111707be32f1cfbc1ab15662
LoanThai <thloanth.dut@gmail.com>
2021-07-08 09:54:55 +0700

    Update home_screen.dart

--- Flutter_Weather/lib/screens/home_screen.dart
+++ Flutter_Weather/lib/screens/home_screen.dart
@@ -1,15 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_weather/packages/dafluta/dafluta.dart';
+import 'package:flutter_weather/models/index.dart';
 import 'package:flutter_weather/resources/assets.dart';
-import 'package:flutter_weather/resources/palette.dart';
-import 'package:flutter_weather/services/date_formatter.dart';
 import 'package:flutter_weather/services/logger.dart';
+import 'package:flutter_weather/storage/json_repositories/weather_data_repository.dart';
 import 'package:flutter_weather/widgets/custom_app_bar.dart';
 import 'package:flutter_weather/widgets/drawers/control_drawer.dart';
 import 'package:flutter_weather/widgets/weather_info_widget.dart';
-import 'package:weather_icons/weather_icons.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -30,6 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    WeatherDataRepository.printJson().then(print);
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: CustomAppBar(
