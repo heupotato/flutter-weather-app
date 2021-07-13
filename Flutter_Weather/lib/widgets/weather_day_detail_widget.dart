ac3e1a6a9fb7bf54a955d8bb3b2c5b1e071c6110
LoanThai <thloanth.dut@gmail.com>
2021-07-13 10:53:30 +0700

    Update weather_day_detail_widget.dart

--- Flutter_Weather/lib/widgets/weather_day_detail_widget.dart
+++ Flutter_Weather/lib/widgets/weather_day_detail_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_weather/models/index.dart';
 import 'package:flutter_weather/packages/dafluta/dafluta.dart';
 import 'package:flutter_weather/storage/json_repositories/weather_data_repository.dart';
+import 'package:flutter_weather/widgets/icons/weather_type_icon.dart';
 import 'package:weather_icons/weather_icons.dart';
 
 class WeatherDayDetail extends StatefulWidget {
@@ -36,69 +37,8 @@ class _WeatherDayDetailState extends State<WeatherDayDetail> {
     );
   }
 
-  _getWeatherDataIcon(String weather) {
-    switch (weather) {
-      case "clearday":
-        return WeatherIcons.day_sunny;
-      case "clearnight":
-        return WeatherIcons.night_clear;
-      case "pcloudyday":
-        return WeatherIcons.day_cloudy_gusts;
-      case "pcloudynight":
-        return WeatherIcons.night_partly_cloudy;
-      case "mcloudyday":
-        return WeatherIcons.day_cloudy_high;
-      case "mcloudynight":
-        return WeatherIcons.night_cloudy_high;
-      case "cloudyday":
-        return WeatherIcons.day_cloudy;
-      case "cloudynight":
-        return WeatherIcons.night_cloudy;
-      case "humidday":
-        return WeatherIcons.day_fog;
-      case "humidnight":
-        return WeatherIcons.night_fog;
-      case "lightrainday":
-        return WeatherIcons.day_rain;
-      case "lightrainnight":
-        return WeatherIcons.night_rain;
-      case "oshowerday":
-        return WeatherIcons.day_showers;
-      case "oshowernight":
-        return WeatherIcons.night_showers;
-      case "ishowerday":
-        return WeatherIcons.day_storm_showers;
-      case "ishowernight":
-        return WeatherIcons.night_storm_showers;
-      case "lightsnowday":
-        return WeatherIcons.day_snow;
-      case "lightsnownight":
-        return WeatherIcons.night_snow;
-      case "rainday":
-        return WeatherIcons.day_rain_mix;
-      case "rainnight":
-        return WeatherIcons.night_rain_mix;
-      case "snowday":
-        return WeatherIcons.day_snow_wind;
-      case "snownight":
-        return WeatherIcons.night_snow_wind;
-      case "rainsnowday":
-        return WeatherIcons.day_snow_thunderstorm;
-      case "rainsnownight":
-        return WeatherIcons.night_snow_thunderstorm;
-      case "tsday":
-        return WeatherIcons.day_lightning;
-      case "tsnight":
-        return WeatherIcons.night_lightning;
-      case "tsrainday":
-        return WeatherIcons.day_thunderstorm;
-      case "tsrainnight":
-        return WeatherIcons.night_thunderstorm;
-    }
-  }
-
-  Icon _weatherIcon(String weather) {
-    return Icon(_getWeatherDataIcon(weather), color: Colors.white, size: 30);
+  WeatherTypeIcon _weatherIcon(String weather) {
+    return WeatherTypeIcon(weather: weather);
   }
 
   List<Widget> WeatherInfoDay(List<WeatherData> dayData, int initTime) {
