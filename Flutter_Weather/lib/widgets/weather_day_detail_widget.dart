a186ee347d92cb753f43e736b70a76c5a454b643
LoanThai <thloanth.dut@gmail.com>
2021-07-13 08:50:44 +0700

    Update weather_day_detail_widget.dart
    Move public functions to private class

--- Flutter_Weather/lib/widgets/weather_day_detail_widget.dart
+++ Flutter_Weather/lib/widgets/weather_day_detail_widget.dart
@@ -22,7 +22,10 @@ class _WeatherDayDetailState extends State<WeatherDayDetail> {
     double pos = (widget.position == 'bottom') ? 0.75 : 0.1;
     return Column(
       children: [
-        VBox(MediaQuery.of(context).size.height*pos),
+        VBox(MediaQuery
+            .of(context)
+            .size
+            .height * pos),
         Row(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
@@ -32,91 +35,90 @@ class _WeatherDayDetailState extends State<WeatherDayDetail> {
       ],
     );
   }
-}
 
-_getWeatherDataIcon(String weather)
-{
-  switch (weather)
-  {
-    case "clearday":
-      return WeatherIcons.day_sunny;
-    case "clearnight":
-      return WeatherIcons.night_clear;
-    case "pcloudyday":
-      return WeatherIcons.day_cloudy_gusts;
-    case "pcloudynight":
-      return WeatherIcons.night_partly_cloudy;
-    case "mcloudyday":
-      return WeatherIcons.day_cloudy_high;
-    case "mcloudynight":
-      return WeatherIcons.night_cloudy_high;
-    case "cloudyday":
-      return WeatherIcons.day_cloudy;
-    case "cloudynight":
-      return WeatherIcons.night_cloudy;
-    case "humidday":
-      return WeatherIcons.day_fog;
-    case "humidnight":
-      return WeatherIcons.night_fog;
-    case "lightrainday":
-      return WeatherIcons.day_rain;
-    case "lightrainnight":
-      return WeatherIcons.night_rain;
-    case "oshowerday":
-      return WeatherIcons.day_showers;
-    case "oshowernight":
-      return WeatherIcons.night_showers;
-    case "ishowerday":
-      return WeatherIcons.day_storm_showers;
-    case "ishowernight":
-      return WeatherIcons.night_storm_showers;
-    case "lightsnowday":
-      return WeatherIcons.day_snow;
-    case "lightsnownight":
-      return WeatherIcons.night_snow;
-    case "rainday":
-      return WeatherIcons.day_rain_mix;
-    case "rainnight":
-      return WeatherIcons.night_rain_mix;
-    case "snowday":
-      return WeatherIcons.day_snow_wind;
-    case "snownight":
-      return WeatherIcons.night_snow_wind;
-    case "rainsnowday":
-      return WeatherIcons.day_snow_thunderstorm;
-    case "rainsnownight":
-      return WeatherIcons.night_snow_thunderstorm;
-    case "tsday":
-      return WeatherIcons.day_lightning;
-    case "tsnight":
-      return WeatherIcons.night_lightning;
-    case "tsrainday":
-      return WeatherIcons.day_thunderstorm;
-    case "tsrainnight":
-      return WeatherIcons.night_thunderstorm;
+  _getWeatherDataIcon(String weather) {
+    switch (weather) {
+      case "clearday":
+        return WeatherIcons.day_sunny;
+      case "clearnight":
+        return WeatherIcons.night_clear;
+      case "pcloudyday":
+        return WeatherIcons.day_cloudy_gusts;
+      case "pcloudynight":
+        return WeatherIcons.night_partly_cloudy;
+      case "mcloudyday":
+        return WeatherIcons.day_cloudy_high;
+      case "mcloudynight":
+        return WeatherIcons.night_cloudy_high;
+      case "cloudyday":
+        return WeatherIcons.day_cloudy;
+      case "cloudynight":
+        return WeatherIcons.night_cloudy;
+      case "humidday":
+        return WeatherIcons.day_fog;
+      case "humidnight":
+        return WeatherIcons.night_fog;
+      case "lightrainday":
+        return WeatherIcons.day_rain;
+      case "lightrainnight":
+        return WeatherIcons.night_rain;
+      case "oshowerday":
+        return WeatherIcons.day_showers;
+      case "oshowernight":
+        return WeatherIcons.night_showers;
+      case "ishowerday":
+        return WeatherIcons.day_storm_showers;
+      case "ishowernight":
+        return WeatherIcons.night_storm_showers;
+      case "lightsnowday":
+        return WeatherIcons.day_snow;
+      case "lightsnownight":
+        return WeatherIcons.night_snow;
+      case "rainday":
+        return WeatherIcons.day_rain_mix;
+      case "rainnight":
+        return WeatherIcons.night_rain_mix;
+      case "snowday":
+        return WeatherIcons.day_snow_wind;
+      case "snownight":
+        return WeatherIcons.night_snow_wind;
+      case "rainsnowday":
+        return WeatherIcons.day_snow_thunderstorm;
+      case "rainsnownight":
+        return WeatherIcons.night_snow_thunderstorm;
+      case "tsday":
+        return WeatherIcons.day_lightning;
+      case "tsnight":
+        return WeatherIcons.night_lightning;
+      case "tsrainday":
+        return WeatherIcons.day_thunderstorm;
+      case "tsrainnight":
+        return WeatherIcons.night_thunderstorm;
+    }
   }
-}
 
-Icon _weatherIcon(String weather){
-  return Icon(_getWeatherDataIcon(weather), color: Colors.white, size: 30);
-}
-
-List<Widget> WeatherInfoDay(List<WeatherData> dayData, int initTime){
-  List<Widget> _weatherInfoDay = [];
-  for (int i = 0; i<10; i++){
-    if (i % 2 == 0) _weatherInfoDay.add(HBox(15));
-    else _weatherInfoDay.add(WeatherInfoHour(dayData[i~/2], initTime));
+  Icon _weatherIcon(String weather) {
+    return Icon(_getWeatherDataIcon(weather), color: Colors.white, size: 30);
   }
-  return _weatherInfoDay;
-}
 
-Container WeatherInfoHour(WeatherData hourData, int initTime){
-  String currentTime = (hourData.timepoint + initTime > 12) ?
-      (hourData.timepoint + initTime - 12).toString() + "PM"
-      : (hourData.timepoint + initTime ).toString() + "AM";
+  List<Widget> WeatherInfoDay(List<WeatherData> dayData, int initTime) {
+    List<Widget> _weatherInfoDay = [];
+    for (int i = 0; i < 10; i++) {
+      if (i % 2 == 0)
+        _weatherInfoDay.add(HBox(15));
+      else
+        _weatherInfoDay.add(WeatherInfoHour(dayData[i ~/ 2], initTime));
+    }
+    return _weatherInfoDay;
+  }
 
-  return Container(
-      height: hourData.temp2m*5,
+  Container WeatherInfoHour(WeatherData hourData, int initTime) {
+    String currentTime = (hourData.timepoint + initTime > 12) ?
+    (hourData.timepoint + initTime - 12).toString() + "PM"
+        : (hourData.timepoint + initTime).toString() + "AM";
+
+    return Container(
+      height: hourData.temp2m * 5,
       width: 60,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
@@ -130,8 +132,8 @@ Container WeatherInfoHour(WeatherData hourData, int initTime){
           Text(
             "${hourData.temp2m}°C",
             style: TextStyle(
-              color: Colors.white,
-              fontSize: 20
+                color: Colors.white,
+                fontSize: 20
             ),
           ),
           VBox(15),
@@ -141,9 +143,10 @@ Container WeatherInfoHour(WeatherData hourData, int initTime){
                 color: Colors.yellow,
                 fontSize: 15
             ),
-          ), 
+          ),
           VBox(15)
-      ],
-  ),
-  );
+        ],
+      ),
+    );
+  }
 }
