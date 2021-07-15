1cde50a3397666e9a14a11e7019696eec12cdc7a
LoanThai <thloanth.dut@gmail.com>
2021-07-15 14:43:08 +0700

    Update weather_extension.dart

--- Flutter_Weather/lib/models/weather_extension.dart
+++ Flutter_Weather/lib/models/weather_extension.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:collection/collection.dart';
 import 'package:flutter_weather/models/index.dart';
-import 'package:flutter_weather/resources/assets.dart';
 import 'package:flutter_weather/services/date_formatter.dart';
 import 'package:flutter_weather/services/logger.dart';
 import 'package:intl/intl.dart';
@@ -275,54 +274,42 @@ extension WeekWeatherExtension on Weather{
   List<String> get weekDays
   => this.getTrueDates(this.weekDayIndexes).map((trueDate) => DateFormatter.weekDay(trueDate)).toList();
 
-  List<int> get weekDayIndexes{
-    List<int> indexes = [];
-    for (int i = 0; i < dataseries.length - 1; i++){
-      if (getTrueDate(dataseries[i].timepoint).day < getTrueDate(dataseries[i + 1].timepoint).day)
-        indexes.add(i);
-    }
-    indexes.add(dataseries.length - 1);
-    indexes = indexes.getRange(0, 8).toList();
-    return indexes;
+  List<DayWeather> allDays() {
+    List<DayWeather> allDays = [];
+    final Map<DateTime, List<WeatherData>> allDaysData =
+      groupBy(dataseries, (WeatherData data) {
+        return this.yearMonthDay(this.getTrueDate(data.timepoint));
+      });
+    allDaysData.forEach((key, value) {allDays.add(DayWeather(weathers: value, initDate: this.initDate)); });
+    return allDays;
   }
 
-  WeekWeather get weekData{
-    final indexList = this.weekDayIndexes;
-    final List<DayWeather> weekData = [];
-    for (int i = 0; i < 8; i++){
-      weekData.add(DayWeather
-        (day: dataseries.getRange((i != 0) ? i - 1 : 0, indexList[i] + 1).toList()));
-    }
-    return WeekWeather(weekData: weekData);
+  List<DayWeather> allAvailableDays(){
+    return this.allDays().where((day) =>
+    DateTime.now().isBefore(this.getTrueDate(day.weathers[0].timepoint))).toList();
   }
+
+  DayWeather get today => this.allAvailableDays().first;
 }
 
 class DayWeather{
-  final List<WeatherData> day;
-  const DayWeather({required this.day});
+  final DateTime initDate;
+  final List<WeatherData> weathers;
+  const DayWeather({required this.weathers, required this.initDate});
 
-  WeatherData nowData(int nowHour, Weather weatherData){
-    WeatherData res = day[day.length - 1];
-    for (int i = 0; i < day.length; i++){
-      int hour = weatherData.getTrueDate(day[i].timepoint).hour;
-      if (hour > nowHour) {
-        int prevHour = weatherData.getTrueDate(day[i - 1].timepoint).hour;
-        if ((hour - nowHour).abs() < (hour - prevHour).abs())
-          return day[i];
-        return day[i -1];
-      }
-    }
-    return res;
+  String weekDay(){
+    return DateFormatter.weekDay(initDate.add(Duration(hours: weathers.first.timepoint)));
+  }
+
+  WeatherData get weatherNow{
+    DateTime now = DateTime.now();
+    return weathers.firstWhere((weatherData) =>
+      now.difference(initDate.add(Duration(hours: weathers.first.timepoint))).inMinutes <= 90);
   }
 
   int get upperLimitTemp
-    => day.map((hourData) => hourData.temp2m).toList().reduce(max);
+    => weathers.map((hourData) => hourData.temp2m).toList().reduce(max);
 
   int get lowerLimitTemp
-    => day.map((hourData) => hourData.temp2m).toList().reduce(min);
-}
-
-class WeekWeather{
-  final List<DayWeather> weekData;
-  const WeekWeather({required this.weekData});
+    => weathers.map((hourData) => hourData.temp2m).toList().reduce(min);
 }
