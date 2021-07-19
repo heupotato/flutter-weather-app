89e54c45ac44da682e85ab6a12bca8ba5f2937b8
LoanThai <thloanth.dut@gmail.com>
2021-07-19 17:26:26 +0700

    Use ListView.separated instead of loop in weatherInfoDay method
    Modified related widgets

--- Flutter_Weather/lib/widgets/weather_info_day.dart
+++ Flutter_Weather/lib/widgets/weather_info_day.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_weather/models/index.dart';
 import 'package:flutter_weather/models/weather_extension.dart';
