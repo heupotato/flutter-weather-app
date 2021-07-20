333ee2696ac312b7daef0b9adcd2c372deda7bb7
LoanThai <thloanth.dut@gmail.com>
2021-07-20 14:55:30 +0700

    Create a timer for appbar label

--- Flutter_Weather/lib/widgets/custom_app_bar.dart
+++ Flutter_Weather/lib/widgets/custom_app_bar.dart
@@ -1,14 +1,38 @@
+import 'dart:async';
+
 import 'package:flutter_weather/resources/palette.dart';
 import 'package:flutter_weather/services/date_formatter.dart';
 import 'package:flutter_weather/widgets/labels/label.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class AppBarLabel extends StatelessWidget {
+class AppBarLabel extends StatefulWidget {
   final String city;
   AppBarLabel({required this.city});
+
+  @override
+  _AppBarLabelState createState() => _AppBarLabelState();
+}
+
+class _AppBarLabelState extends State<AppBarLabel> {
+  late String _timeString;
+  @override
+  void initState(){
+    _timeString = DateFormatter.dateTime(DateTime.now());
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+    super.initState();
+  }
+  
+  void _getTime() {
+    final String formattedDateTime = DateFormatter.dateTime(DateTime.now());
+    setState(() {
+      _timeString = formattedDateTime;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    String _city = widget.city;
     return RichText(
         text: TextSpan(
             children: [
@@ -16,7 +40,7 @@ class AppBarLabel extends StatelessWidget {
                   text:"Da Nang City",
                   style: TextStyle(fontSize: 25)
               ),
-              TextSpan(text: "\n${DateFormatter.dateTime(DateTime.now())}")
+              TextSpan(text: "\n$_timeString")
             ]
         )
     );
