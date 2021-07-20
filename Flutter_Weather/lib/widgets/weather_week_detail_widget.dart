96cdf95063d44b5393788c54df852b89f3e0e45b
LoanThai <thloanth.dut@gmail.com>
2021-07-20 15:52:45 +0700

    Remove divider under ExpansionTile

--- Flutter_Weather/lib/widgets/weather_week_detail_widget.dart
+++ Flutter_Weather/lib/widgets/weather_week_detail_widget.dart
@@ -63,66 +63,31 @@ class _WeatherWeekDetailState extends State<WeatherWeekDetail> {
             flex4: 2, child4: Text("Low", style: titleStyle)));
   }
 
-  List<CustomExpansionTile> weatherRows(List<DayWeather> allAvailableDays){
+  List<Theme> weatherRows(List<DayWeather> allAvailableDays){
     return allAvailableDays.map((dayWeather) => weatherRow(dayWeather)).toList();
   }
 
-  CustomExpansionTile weatherRow(DayWeather dayWeather){
+  Theme weatherRow(DayWeather dayWeather){
     WeatherInfoDay weatherInfoDay = WeatherInfoDay(dayData: dayWeather);
-    return CustomExpansionTile(
-      tilePadding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-      childrenPadding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-      backgroundColor: Palette.transparent,
-      title: CustomFlexRow(
-        flex1: 3, child1: FittedBox(
-          alignment: Alignment.centerLeft,
-          fit: BoxFit.scaleDown,
-          child: Text(dayWeather.weekDay, style: cellStyle, textAlign: TextAlign.left)),
-        flex2: 3, child2: WeatherTypeIcon(weather: dayWeather.typicalWeather),
-        flex3: 2, child3: Text("${dayWeather.upperLimitTemp}°", style: cellStyle),
-        flex4: 2, child4: Text("${dayWeather.lowerLimitTemp}°", style: cellStyle)),
-      children: [
-        Padding(
-        padding: EdgeInsets.fromLTRB(0,0,0, 0),
-          child: weatherInfoDay)
-      ]
-    );
+    return Theme(
+      data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+      child: CustomExpansionTile(
+        tilePadding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+        childrenPadding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+        backgroundColor: Palette.transparent,
+        title: CustomFlexRow(
+          flex1: 3, child1: FittedBox(
+            alignment: Alignment.centerLeft,
+            fit: BoxFit.scaleDown,
+            child: Text(dayWeather.weekDay, style: cellStyle, textAlign: TextAlign.left)),
+          flex2: 3, child2: WeatherTypeIcon(weather: dayWeather.typicalWeather),
+          flex3: 2, child3: Text("${dayWeather.upperLimitTemp}°", style: cellStyle),
+          flex4: 2, child4: Text("${dayWeather.lowerLimitTemp}°", style: cellStyle)),
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(0,0,0, 0),
+            child: weatherInfoDay)
+        ]
+      ));
   }
-  //     Table(
-  //       border: TableBorder.symmetric(),
-  //       columnWidths: {
-  //         0: FlexColumnWidth(4),
-  //         1: FlexColumnWidth(3),
-  //         2: FlexColumnWidth(1.5),
-  //         3: FlexColumnWidth(1.5)
-  //       },
-  //       children: [
-  //         TableRow(
-  //           children: [
-  //             TableCell(child: SizedBox(height: 30, child: Text(""))),
-  //             TableCell(child: SizedBox(height: 30, child: Text(""))),
-  //             TableCell(child: SizedBox(height: 30, child: Text("High", style: titleStyle))),
-  //             TableCell(child: SizedBox(height: 30, child: Text("Low", style: titleStyle)))
-  //           ],
-  //         ),
-  //         ...weatherRows(allAvailableDays)
-  //       ],
-  //     ),
-  //   );
-  // }
-  //
-  // List<TableRow> weatherRows(List<DayWeather> allAvailableDays){
-  //   return allAvailableDays.map((weatherDay) => weatherRow(weatherDay)).toList();
-  // }
-  //
-  // TableRow weatherRow(DayWeather weatherDay){
-  //   return TableRow(
-  //     children: [
-  //       TableCell(child: SizedBox(height: 40, child: Text(weatherDay.weekDay, style: cellStyle))),
-  //       TableCell(child: SizedBox(height: 40, child:WeatherTypeIcon(weather: weatherDay.typicalWeather))),
-  //       TableCell(child: SizedBox(height: 40, child:Text("${weatherDay.upperLimitTemp}°", style: cellStyle))),
-  //       TableCell(child: SizedBox(height: 40, child:Text("${weatherDay.lowerLimitTemp}°", style: cellStyle)))
-  //     ]
-  //   );
-  // }
 }
