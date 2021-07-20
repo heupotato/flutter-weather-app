5241488ded1f1ac23b30564fdac1d2b99283ca22
LoanThai <thloanth.dut@gmail.com>
2021-07-20 14:17:56 +0700

    Update weather_extension.dart

--- Flutter_Weather/lib/models/weather_extension.dart
+++ Flutter_Weather/lib/models/weather_extension.dart
@@ -306,10 +306,6 @@ class DayWeather{
     DateTime now = DateTime.now().toUtc();
     WeatherData weatherNow = weathers.firstWhere((weatherData) =>
       now.difference(initDate.add(Duration(hours: weatherData.timepoint)).toUtc()).abs().inMinutes <= 90);
-    print(now);
-    DateTime chosenDate = initDate.add(Duration(hours: weatherNow.timepoint));
-    print(chosenDate);
-    print(now.difference(chosenDate).abs().inMinutes);
     return weatherNow;
   }
 
