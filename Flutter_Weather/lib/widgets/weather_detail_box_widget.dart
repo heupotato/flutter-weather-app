62eed715c63ced205f6fc08a91cc996876e35c23
LoanThai <thloanth.dut@gmail.com>
2021-07-21 16:33:52 +0700

    Update weather_detail_box_widget.dart
    Change params due to previous changes

--- Flutter_Weather/lib/widgets/weather_detail_box_widget.dart
+++ Flutter_Weather/lib/widgets/weather_detail_box_widget.dart
@@ -5,8 +5,8 @@ import 'package:flutter_weather/packages/dafluta/dafluta.dart';
 import 'package:flutter_weather/widgets/icons/weather_type_icon.dart';
 
 class WeatherDetailBox extends StatelessWidget {
-  final DayWeather todayWeather;
-  WeatherDetailBox({Key? key, required this.todayWeather}) : super(key: key);
+  final List<DayWeather> weekWeather;
+  WeatherDetailBox({Key? key, required this.weekWeather}) : super(key: key);
 
   final TextStyle titleStyle = TextStyle(
       color: Colors.white,
@@ -21,6 +21,10 @@ class WeatherDetailBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DayWeather todayWeather = weekWeather.first;
+    DayWeather ? tomorrowWeather = (weekWeather.length > 1) ? weekWeather[1] : null;
+    List<Widget> weatherText = (DateTime.now().hour < 18) ? List.from(dayTimeDetail(todayWeather, tomorrowWeather))
+                                : List.from(nightDetail(todayWeather, tomorrowWeather));
     return Container(
       padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
       margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
@@ -51,21 +55,18 @@ class WeatherDetailBox extends StatelessWidget {
                     child: WeatherTypeIcon(weather: todayWeather.weatherNow.weather),
                     alignment: Alignment.center,)),
                   Text(""),
-                  TableCell(child: tableInfo())
+                  TableCell(child: tableInfo(todayWeather))
                 ]
               )
             ],
           ),
-          VBox(15),
-          todayText(),
-          VBox(15),
-          tonightText()
+          ...weatherText
         ],
       ),
     );
   }
 
-  Table tableInfo(){
+  Table tableInfo(DayWeather todayWeather){
     final WeatherData typicalWeather = todayWeather.typicalWeather;
     return Table(
       border: TableBorder.symmetric(),
@@ -102,10 +103,10 @@ class WeatherDetailBox extends StatelessWidget {
     );
   }
 
-  Text todayText(){
+  Text todayText(DayWeather todayWeather){
     WeatherData todayData = todayWeather.todayTypicalWeather;
     return Text(
-      "Today is ${todayData.weather} with a high of ${todayData.temp2m}° "
+      "Today is ${todayData.weather} with a high of ${todayWeather.todayMaxTemp}° "
           "and a ${todayData.precipitation} chance of precipitation. "
           "Winds ${todayWeather.todayWindDirection} at ${todayData.windSpeed}",
       style: TextStyle(
@@ -115,16 +116,52 @@ class WeatherDetailBox extends StatelessWidget {
     );
   }
 
-  Text tonightText(){
-    WeatherData tonightData = todayWeather.tonightTypicalWeather;
+  Text tonightText(DayWeather todayWeather, DayWeather ? tomorrowWeather){
+    WeatherData tonightData = todayWeather.tonightTypicalWeather(tomorrowWeather);
     return Text(
         "Tonight is ${tonightData.weather}. "
-            "Winds ${todayWeather.tonightWindDirection} at ${tonightData.windSpeed}."
-            "The overnight low will be ${tonightData.temp2m}°.",
+            "Winds ${todayWeather.tonightWindDirection(tomorrowWeather)} at ${tonightData.windSpeed}."
+            "The overnight low will be ${todayWeather.tonightMinTemp(tomorrowWeather)}°.",
       style: TextStyle(
           color: Colors.white,
           fontSize: 15
       ),
     );
+  }
+
+  Text tomorrowText(DayWeather ? tomorrowWeather){
+    if (tomorrowWeather == null)
+      return Text("There isn't any weather forecast for tomorrow",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 15));
+    WeatherData tomorrowData = tomorrowWeather.todayTypicalWeather;
+    return Text(
+      "Tomorrow will be ${tomorrowData.weather} with a high of ${tomorrowWeather.todayMaxTemp}° "
+          "and a ${tomorrowData.precipitation} chance of precipitation. "
+          "Winds ${tomorrowWeather.todayWindDirection} at ${tomorrowData.windSpeed}",
+      style: TextStyle(
+          color: Colors.white,
+          fontSize: 15
+      ),
+    );
+  }
+
+  List<Widget> dayTimeDetail(DayWeather todayWeather, DayWeather ? tomorrowWeather){
+    return[
+      VBox(15),
+      todayText(todayWeather),
+      VBox(15),
+      tonightText(todayWeather, tomorrowWeather)
+    ];
+  }
+
+  List<Widget> nightDetail(DayWeather todayWeather, DayWeather ? tomorrowWeather){
+    return [
+      VBox(15),
+      tonightText(todayWeather, tomorrowWeather),
+      VBox(15),
+      tomorrowText(tomorrowWeather)
+    ];
   }
 }
