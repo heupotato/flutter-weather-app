1688b06a664a7eaedb08a35cc9028855a7ffea19
LoanThai <thloanth.dut@gmail.com>
2021-07-28 17:44:59 +0700

    Local Storage

--- Flutter_Weather/lib/storage/local_storage/abstract_local_storage.dart
+++ Flutter_Weather/lib/storage/local_storage/abstract_local_storage.dart
@@ -21,6 +21,7 @@ abstract class AbstractLocalStorage<T> {
     final directory = await getApplicationDocumentsDirectory();
     final appDataDirectory = Directory(directory.path + '/app_data');
     if (await appDataDirectory.exists()) {
+      print(appDataDirectory.path);
       return appDataDirectory.path;
     } else {
       await appDataDirectory.create();
@@ -47,6 +48,7 @@ abstract class AbstractLocalStorage<T> {
       final file = await localFile;
       final contents = await file.readAsString();
       final List<dynamic> jsons = json.decode(contents);
+      print(jsons);
       return fromJsonList!(jsons);
     } catch (e) {
       print("readItems err msg" + e.toString());
