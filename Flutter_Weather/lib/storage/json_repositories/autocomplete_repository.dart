79cbe99f2126445afe0b46ecacdf3d7709daca64
LoanThai <thloanth.dut@gmail.com>
2021-08-03 17:41:17 +0700

    Update autocomplete_repository.dart
    Add GetAutocompleteData class for http requests

--- Flutter_Weather/lib/storage/json_repositories/autocomplete_repository.dart
+++ Flutter_Weather/lib/storage/json_repositories/autocomplete_repository.dart
@@ -2,14 +2,29 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_weather/models/index.dart';
+import 'package:flutter_weather/packages/dahttp/dahttp.dart';
 import 'package:flutter_weather/resources/assets.dart';
+import 'package:http/src/response.dart';
 
 
 class AutoCompleteRepository{
-  static Future<Autocomplete> getData() async{
+  static Future<Autocomplete> getMockData() async{
     final response = await rootBundle.loadString(Assets.mockAutocompleteData);
     final data = await jsonDecode(response);
     Autocomplete mockAutocomplete = Autocomplete.fromJson(data);
     return mockAutocomplete;
   }
+}
+
+class GetAutocompleteData extends ValuedHttpClient<Autocomplete>{
+  Future<HttpResult<Autocomplete>> call(String place)
+  => super.get('https://api.mapbox.com/geocoding/v5/mapbox.places/$place.json?access_token=pk.eyJ1IjoiaGV1cG90YXRvIiwiYSI6ImNrcmU5M3ppMDF6bnMybmxxdWZybnhnZ2EifQ.HipNHpiJeyatsN3dj4-zvA');
+
+  @override
+  Autocomplete convert(Response response) {
+    final data = jsonDecode(response.body);
+    Autocomplete autoCompleteData = Autocomplete.fromJson(data);
+    return autoCompleteData;
+  }
+
 }
